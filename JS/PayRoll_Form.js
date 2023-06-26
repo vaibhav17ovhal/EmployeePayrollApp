8a9ed2ed089f4e93cc0cee76374fab04ccb78938
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-26 20:32:59 +0530

    [Vaibhav] Add:Validation Employee Payroll Data

--- JS/PayRoll_Form.js
+++ JS/PayRoll_Form.js
@@ -1,55 +1,74 @@
-class EmployeePayrollData{
-    get id() { return this._id; }
-    set id(id) {
-        this._id = id
-    }
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input' , function(){
+        if(name.value.length == 0){
+            textError.textContent = '';
+            return
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = '';
+        }
+        catch(e){
+            textError.textContent = e;
+        }
+    });
+    const salary = document.querySelector('#salary');
+    const salaryOutput = document.querySelector('.salary-output-text');
+    salaryOutput.textContent = salary.value;
+    salary.addEventListener('input', function(){
+        salaryOutput.textContent = salary.value;
+    });
+});
 
-    get name() { return this._name; }
-    set name(name) {
-        let nameRegex = RegExp('^[A-Z][A-Za-z]{2,}$');
-        if(nameRegex.test(name))
-            this._name = name;
-        else throw "Name is Incorrect";
-    }
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        creatingAndUpdateStorage(employeePayrollData);
 
-    get profilePic() { return this._profilePic; }
-    set profilePic(profilePic) {
-        this._profilePic = profilePic;
     }
-
-    get gender() { return this._gender; }
-    set gender(gender) {
-        this._gender = gender;
-    }
-
-    get department() { return this._department; }
-    set department(department) {
-        this._department = department;
-    }
-
-    get salary() { return this._salary; }
-    set salary(salary) {
-        this._salary = salary;
-    }
-
-    get note() { return this._note; }
-    set note(note) {
-        this._note = note;
-    }
-
-    get startDate() { return this._startDate }
-    set startDate(startDate) {
-        this._startDate = startDate;
-    }
-
-    toString() {
-        return "id = " + this.id + ", name = " + this.name + ", gender = " + this.gender + ", profilePic = " + this.profilePic + ", department = " + this.department + ", salary = " + this.salary + ", startDate = " + this.startDate + ", note = " + this.note
+    catch (e) {
+        return;
     }
 }
 
-const salary = document.querySelector('#salary');
-const salaryOutput = document.querySelector('.salary-output-text');
-salaryOutput.textContent = salary.value;
-salary.addEventListener('input', function(){
-    salaryOutput.textContent = salary.value;
-});
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+        setTextValue('.text-error', e)
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name = profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name = gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name = department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert("Employee Payroll date is populated in object: " + employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyvalue) => {
+    let allItems = document.querySelectorAll(propertyvalue);
+    let setter = [];
+    allItems.forEach(item => {
+        if (item.checked) setter.push(item.value);
+    });
+    return setter;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
